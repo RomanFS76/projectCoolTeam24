508c0b384be07b6fcb23cd2eae70bee0b2848b61
RomanFS76 <korvy85@gmail.com>
2024-01-18 16:24:06 +0200

    js open-close mob-menu

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let burgerMenu = document.querySelector('.burger-menu');
+let backdrop = document.querySelector('.backdrop');
+let closeButton = document.querySelector('.close-button');
+
+burgerMenu.addEventListener('click', openMobMenu);
+
+function openMobMenu () {
+    backdrop.classList.toggle('is-open');
+}
+
+closeButton.addEventListener('click', closeMobMenu);
+
+function closeMobMenu () {
+    backdrop.classList.toggle('is-open');
+}
+
